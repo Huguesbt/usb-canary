4dbcbbd7939201865ff88d47111b89434466bbcf
Huguesbt <45922268+Huguesbt@users.noreply.github.com>
2026-01-06 22:10:00 +0100

    update

--- functions.sh
+++ functions.sh
@@ -14,15 +14,15 @@ send_slack(){
     data="{\
         \"attachments\": [\
             {\
-                \"color\": \"$SLACK_COLOR\",\
-                \"author_name\": \"$SLACK_BOT\",\
-                \"title\": \"$SLACK_SUBJECT\",\
-                \"text\": \"$message\"\
+                \"color\": \"${SLACK_COLOR}\",\
+                \"author_name\": \"${SLACK_BOT}\",\
+                \"title\": \"${SLACK_SUBJECT}\",\
+                \"text\": \"${message}\"\
             }\
         ]\
     }"
 
-    curl -X POST -H 'Content-type: application/json' --silent --data "$data" "$SLACK_URL"
+    curl -X POST -H 'Content-type: application/json' --silent --data "${data}" "${SLACK_URL}"
 }
 
 send_twilio(){
@@ -35,29 +35,43 @@ send_twilio(){
         [[ "$message" == "" ]]; then
             exit 0
     fi
-    url="$TWILIO_URL"
-    curl -X POST "$url" \
-        --data-urlencode "Body=$message" \
-        --data-urlencode "From=$TWILIO_FROM" \
-        --data-urlencode "To=$TWILIO_TO" \
-        -u $TWILIO_SID:$TWILIO_AUTH
+    url="${TWILIO_URL}"
+    curl -X POST "${url}" \
+        --data-urlencode "Body=${message}" \
+        --data-urlencode "From=${TWILIO_FROM}" \
+        --data-urlencode "To=${TWILIO_TO}" \
+        -u ${TWILIO_SID}:${TWILIO_AUTH}
 }
 
 send_notif() {
     if [[ ! -z ${NOTIF_BIN+x} ]]; then
         if [[ -z ${NOTIF_LEVEL+x} ]]; then NOTIF_LEVEL="normal"; fi
-        ${NOTIF_BIN} --urgency="$NOTIF_LEVEL" "$1"
+        ${NOTIF_BIN} --urgency="${NOTIF_LEVEL}" "${1}"
     fi
+}
+
+send_email(){
+    body=$1
+    if [[ -z ${SMTP+x} ]] || \
+        [[ -z ${EMAIL+x} ]] || \
+        [[ -z ${SUBJECT+x} ]] || \
+        [[ "${body}" == "" ]]; then
+            exit 0
+    fi
+
+    echo ${body} | mail -s "${SUBJECT}" "${EMAIL}"
+
 }
 
 alert() {
     echo "$1"
-    if [[ ! -z ${TWILIO+x} ]] && [[ "$TWILIO" == "1" ]]; then send_twilio "$1"; fi
-    if [[ ! -z ${SLACK+x} ]] && [[ "$SLACK" == "1" ]]; then send_slack "$1"; fi
-    if [[ ! -z ${NOTIF+x} ]] && [[ "$NOTIF" == "1" ]]; then send_notif "$1"; fi
+    if [[ ! -z ${SMTP+x} ]] && [[ "${SMTP}" == "1" ]]; then send_email "$1"; fi
+    if [[ ! -z ${TWILIO+x} ]] && [[ "${TWILIO}" == "1" ]]; then send_twilio "$1"; fi
+    if [[ ! -z ${SLACK+x} ]] && [[ "${SLACK}" == "1" ]]; then send_slack "$1"; fi
+    if [[ ! -z ${NOTIF+x} ]] && [[ "${NOTIF}" == "1" ]]; then send_notif "$1"; fi
 }
 
 exit_canary() {
-  alert "EXIT from USB CANARY on $HOSTNAME"
+  alert "EXIT from USB CANARY on ${HOSTNAME}"
   exit
 }
